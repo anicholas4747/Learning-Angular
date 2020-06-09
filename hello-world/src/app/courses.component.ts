c3c5138228eb4d2b8504908fbe1d987849bf4181
anicholas4747 <an1720@nyu.edu>
2020-06-09 17:20:23 -0400

    services

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CoursesService } from './courses.service';
 
 @Component({
   selector: "courses",
@@ -15,8 +16,11 @@ import { Component } from '@angular/core';
 })
 export class CoursesComponent {
   title = "List of Courses";
-  courses = ["c1", "c2", "c3"];
+  courses;
 
+  constructor (service: CoursesService) {
+    this.courses = service.getCourses();
+  }
   getTitle () {
     return this.title;
   }
