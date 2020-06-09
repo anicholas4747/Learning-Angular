cfad7ee33a060d11c8e9d5690aa1fd0643d94e40
anicholas4747 <an1720@nyu.edu>
2020-06-09 15:48:58 -0400

    imp exp classes in ts

---
+++ ts-hello/test_main.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var point_class_1 = require("./point_class");
+var p = new point_class_1["default"](1, 2);
+p.draw();
---
+++ ts-hello/test_main.ts
@@ -0,0 +1,4 @@
+import Point from "./point_class";
+
+let p = new Point(1,2);
+p.draw();
