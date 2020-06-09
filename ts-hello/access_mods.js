1293e2e30dd510618a72bc59d77425b1dea36def
anicholas4747 <an1720@nyu.edu>
2020-06-09 15:43:04 -0400

    access modifiers

--- ts-hello/access_mods.js
+++ ts-hello/access_mods.js
@@ -1,15 +1,36 @@
 var Point = /** @class */ (function () {
     // the public/private keywords initialize the fields and set access to them at the same time in the constructor
-    function Point(x, y) {
-        this.x = x;
-        this.y = y;
+    function Point(_x, _y) {
+        this._x = _x;
+        this._y = _y;
     }
+    Object.defineProperty(Point.prototype, "x", {
+        // attr_reader to read private field in Point class
+        get: function () {
+            return this._x;
+        },
+        // attr_writer to write to a private field in Point class
+        set: function (newX) {
+            if (newX < 0)
+                throw new Error("value cannot be less than 0");
+            this._x = newX;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Point.prototype, "y", {
+        get: function () {
+            return this._y;
+        },
+        enumerable: false,
+        configurable: true
+    });
     Point.prototype.draw = function () {
         console.log("X: " + this.x + ", Y: " + this.y);
     };
     return Point;
 }());
 var pointA = new Point(1, 2);
-console.log(pointA.x);
-// console.log(pointA.y); // y is private and inaccessible
+var xCoord = pointA.x; // x is a property of this point object and the getter method allows you read access
+// pointA._x = 47; // _x is a field of this point object and cannot be set (since it's private), so this line would throw an error
 pointA.draw();
--- ts-hello/access_mods.ts
+++ ts-hello/access_mods.ts
@@ -1,6 +1,23 @@
 class Point {
   // the public/private keywords initialize the fields and set access to them at the same time in the constructor
-  constructor(public x?: number, private y?: number) {
+  constructor(private _x?: number, private _y?: number) {
+  }
+
+  // attr_reader to read private field in Point class
+  get x () {
+    return this._x;
+  }
+
+  get y () {
+    return this._y;
+  }
+
+  // attr_writer to write to a private field in Point class
+  set x (newX) {
+    if (newX < 0)
+      throw new Error("value cannot be less than 0");
+
+    this._x = newX;
   }
 
   draw () {
@@ -9,6 +26,6 @@ class Point {
 }
 
 let pointA = new Point(1, 2);
-console.log(pointA.x);
-// console.log(pointA.y); // y is private and inaccessible
+let xCoord = pointA.x; // x is a property of this point object and the getter method allows you read access
+// pointA._x = 47; // _x is a field of this point object and cannot be set (since it's private), so this line would throw an error
 pointA.draw();
